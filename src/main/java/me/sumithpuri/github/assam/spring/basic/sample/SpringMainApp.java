7c37cc17ae97003878d4aa7cfcf2278490bac697
sumithpuri <sumith.puri@yahoo.com>
2018-11-28 19:11:33 +0530

    initial check-in of assam
    >brainbench spring 2.5 certification
    #code samples created for above
    #core spring (bean factory)
    #core spring (application context)
    #demonstrates core spring basics
    +includes maven pom.xml
    +initial readme, license, gitignore
    #[tested using core java 9.0+]
    #[tested on windows 10]
    #[should work on *nix, not tested]

---
+++ src/main/java/me/sumithpuri/github/assam/spring/basic/sample/SpringMainApp.java
@@ -0,0 +1,49 @@
+package me.sumithpuri.github.assam.spring.basic.sample;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+
+/**
+ * MIT License
+ *
+ * Copyright (c) 2018-19,	Sumith Kumar Puri
+
+ * GitHub URL 			https://github.com/sumithpuri
+ * Code Sample			Brainbench Spring 2.5 Certification [+ Spring In Action Samples]
+ * Sample Topic			Core Spring (Basics, Dependency Injection)
+ * Certificate URL		https://goo.gl/X321kd
+ * Package Prefix 		me.sumithpuri.github.assam
+ * Project Codename		assam
+ * Contact E-Mail		code@sumithpuri.me
+ * Contact WhatsApp		+91 9591497974
+ *
+ *
+ * Permission is hereby  granted,  free  of  charge, to  any person  obtaining a  copy of  this  software and associated 
+ * documentation files (the "Software"), to deal in the  Software without  restriction, including without limitation the 
+ * rights to use, copy, modify, merge, publish, distribute, sub-license and/or sell copies of the Software and to permit 
+ * persons to whom the Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in  all copies or substantial portions of the 
+ * Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES  OR  OTHER  LIABILITY, WHETHER IN AN ACTION  OF  CONTRACT, TORT OR 
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+public class SpringMainApp {
+
+	public static void main(String[] args) throws Exception {
+
+		demoCoreSpringBasicDI();
+	}
+	
+	public static void demoCoreSpringBasicDI() {
+		
+		BeanFactory factory = new XmlBeanFactory(new ClassPathResource("spring-sample.xml"));
+		SpringSampleIf springService = (SpringSampleIf) factory.getBean("springSampleService");
+		springService.whereIsJesus();
+	}
+}
